fdf4b6837e82dfcf562f7e8b46d669bd51b01ee2
Regan Farren <regan.farren@gmail.com>
2020-01-21 16:17:37 +0000

    updatel;
    Changelog:
    
    Added: Ending_H (Cheating in the game)

---
+++ 3rdYearIndi/Assets/Scripts/CheaterDeath.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(KonamiCode))]
+public class CheaterDeath : MonoBehaviour
+{
+    private KonamiCode code;
+
+    public Scene end_h;
+
+    [SerializeField]
+    private string sceneNameToLoad;
+
+    void Awake()
+    {
+        code = GetComponent<KonamiCode>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (code.success)
+        {
+            //Regan change the level here
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+
+    }
+}
