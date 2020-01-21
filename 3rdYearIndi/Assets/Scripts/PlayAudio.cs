2b2bab79cad3d362671edd53d6944cc197bd9f8b
Regan Farren <regan.farren@gmail.com>
2020-01-21 15:32:17 +0000

    update; Audio
    Changelog:
    
    Added: Audio Script that plays narrartion only once

---
+++ 3rdYearIndi/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+
+    public AudioClip SoundGoesHere;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundGoesHere, Volume);
+            alreadyPlayed = true;
+        }
+    }
+}
